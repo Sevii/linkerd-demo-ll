cf7ac226c707843060b2dfbc1cce3a2c58a5ea62
Sevii <nicksledgian@gmail.com>
2017-03-05 23:24:08 -0700

    Got a working prometheus config setup There is a hard requirement on a service annotation (not label) prometheus.io/scrape=true in Kubernetes

---
+++ kube-dns-test/forward-prometheus.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kubectl get pods -l app=prometheus -o name | sed 's/^.*\///' |  xargs -I{} kubectl port-forward {} 9090:9090
--- kube-dns-test/load-generator.sh
+++ kube-dns-test/load-generator.sh
@@ -4,15 +4,18 @@ edgeUrl=$(eval minikube ip)
 edgePort=$(kubectl get service edge -o=json | jq '.spec.ports[0].nodePort')
 printf "%s\n" "$edgeUrl"
 
-http -a Admin:admin "${edgeUrl}:${edgePort}"
-http -a Guest:password "${edgeUrl}:${edgePort}"
-http -a Stern:mrpaulstern "${edgeUrl}:${edgePort}"
+for ((i=1;i<=20;i++)); do 
+	http -a Admin:admin "${edgeUrl}:${edgePort}";
+	http -a Guest:pass23word "${edgeUrl}:${edgePort}";
+	http -a Ad2min:admin "${edgeUrl}:${edgePort}";
+	http -a Guest:password "${edgeUrl}:${edgePort}";
 
-http -a Admin:ad23in "${edgeUrl}:${edgePort}"
-http -a Guest:pass23word "${edgeUrl}:${edgePort}"
-http -a Stern:mrp23aulstern "${edgeUrl}:${edgePort}"
+	http -a Admin:ad23in "${edgeUrl}:${edgePort}";
+	http -a Stern:mrp23aulstern "${edgeUrl}:${edgePort}";
+	http -a Guefst:password "${edgeUrl}:${edgePort}";
 
+	http -a Stern:mrpaulstern "${edgeUrl}:${edgePort}";
 
-http -a Ad2min:admin "${edgeUrl}:${edgePort}"
-http -a Guefst:password "${edgeUrl}:${edgePort}"
-http -a Stsern:mrpaulstern "${edgeUrl}:${edgePort}"
+	http -a Stsern:mrpaulstern "${edgeUrl}:${edgePort}";
+	sleep 1;
+ done
