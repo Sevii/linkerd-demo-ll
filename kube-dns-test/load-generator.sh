2b77b933c1023a37f3b57426e1ccd817871c2715
Sevii <nicksledgian@gmail.com>
2017-03-05 22:20:14 -0700

    Added prometheus yaml specs and load-generator bash script

---
+++ kube-dns-test/load-generator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+edgeUrl=$(eval minikube ip)
+edgePort=$(kubectl get service edge -o=json | jq '.spec.ports[0].nodePort')
+printf "%s\n" "$edgeUrl"
+
+http -a Admin:admin "${edgeUrl}:${edgePort}"
+http -a Guest:password "${edgeUrl}:${edgePort}"
+http -a Stern:mrpaulstern "${edgeUrl}:${edgePort}"
+
+http -a Admin:ad23in "${edgeUrl}:${edgePort}"
+http -a Guest:pass23word "${edgeUrl}:${edgePort}"
+http -a Stern:mrp23aulstern "${edgeUrl}:${edgePort}"
+
+
+http -a Ad2min:admin "${edgeUrl}:${edgePort}"
+http -a Guefst:password "${edgeUrl}:${edgePort}"
+http -a Stsern:mrpaulstern "${edgeUrl}:${edgePort}"
